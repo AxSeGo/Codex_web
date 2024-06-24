3935f9a71d8b731862b454e6bdcee47e22913688
AxSeGo <axel.seara.gomez@students.beaminstitute.org>
2024-06-24 18:33:37 +0200

    tailwind config done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className="text-center mt-10">
+        <a href="https://vitejs.dev" target="_blank" className="mx-5">
+          <img src={viteLogo} className="logo animate-spin-slow h-20" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" className="mx-5">
+          <img src={reactLogo} className="logo react h-20" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-5xl text-blue-300 font-bold underline">
+        Vite + React
+      </h1>
+      <div className="card bg-gray-100 shadow-xl p-5 m-5">
+        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
+          onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <p className="mt-3">
+          Edit <code className="bg-gray-200 rounded p-1">src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-lg text-gray-600 mt-5">
         Click on the Vite and React logos to learn more
       </p>
     </>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx}",  // Esto incluye todos los archivos .js y .jsx en tu directorio src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
